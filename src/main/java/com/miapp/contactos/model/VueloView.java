1f6d8498933ae018f450cda34cbfeb7b7e0e8651
davidperezmartinez06 <da.pemar06duna@digitechfp.com>
2026-02-25 17:45:53 +0100

    final fix change vuelosview

--- src/main/java/com/miapp/contactos/model/VueloView.java
+++ src/main/java/com/miapp/contactos/model/VueloView.java
@@ -2,10 +2,11 @@ package com.miapp.contactos.model;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "vista_vuelos")
-public class VueloView {
+public class VueloView implements Serializable {
 
     @Id
     @Column(name = "id_vuelo")
@@ -14,7 +15,10 @@ public class VueloView {
     @Column(name = "codigo_vuelo")
     private String codigoVuelo;
 
+    @Column(name = "compania")
     private String compania;
+
+    @Column(name = "avion")
     private String avion;
 
     @Column(name = "ciudad_origen")
@@ -23,7 +27,10 @@ public class VueloView {
     @Column(name = "ciudad_destino")
     private String ciudadDestino;
 
+    @Column(name = "terminal")
     private Integer terminal;
+
+    @Column(name = "puerta")
     private String puerta;
 
     @Column(name = "hora_salida")
@@ -32,26 +39,25 @@ public class VueloView {
     @Column(name = "hora_llegada")
     private LocalDateTime horaLlegada;
 
+    // Getters y Setters
     public Integer getIdVuelo() { return idVuelo; }
-    public String getCodigoVuelo() { return codigoVuelo; }
-    public String getCompania() { return compania; }
-    public String getAvion() { return avion; }
-    public String getCiudadOrigen() { return ciudadOrigen; }
-    public String getCiudadDestino() { return ciudadDestino; }
-    public Integer getTerminal() { return terminal; }
-    public String getPuerta() { return puerta; }
-    public LocalDateTime getHoraSalida() { return horaSalida; }
-    public LocalDateTime getHoraLlegada() { return horaLlegada; }
-
-
     public void setIdVuelo(Integer idVuelo) { this.idVuelo = idVuelo; }
+    public String getCodigoVuelo() { return codigoVuelo; }
     public void setCodigoVuelo(String codigoVuelo) { this.codigoVuelo = codigoVuelo; }
+    public String getCompania() { return compania; }
     public void setCompania(String compania) { this.compania = compania; }
+    public String getAvion() { return avion; }
     public void setAvion(String avion) { this.avion = avion; }
+    public String getCiudadOrigen() { return ciudadOrigen; }
     public void setCiudadOrigen(String ciudadOrigen) { this.ciudadOrigen = ciudadOrigen; }
+    public String getCiudadDestino() { return ciudadDestino; }
     public void setCiudadDestino(String ciudadDestino) { this.ciudadDestino = ciudadDestino; }
+    public Integer getTerminal() { return terminal; }
     public void setTerminal(Integer terminal) { this.terminal = terminal; }
+    public String getPuerta() { return puerta; }
     public void setPuerta(String puerta) { this.puerta = puerta; }
+    public LocalDateTime getHoraSalida() { return horaSalida; }
     public void setHoraSalida(LocalDateTime horaSalida) { this.horaSalida = horaSalida; }
+    public LocalDateTime getHoraLlegada() { return horaLlegada; }
     public void setHoraLlegada(LocalDateTime horaLlegada) { this.horaLlegada = horaLlegada; }
 }
