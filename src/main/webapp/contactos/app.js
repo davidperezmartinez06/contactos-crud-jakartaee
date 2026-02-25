2e9317869a09108907dc642038b14b6d827515ad
davidperezmartinez06 <da.pemar06duna@digitechfp.com>
2026-02-25 16:05:56 +0100

    fix change in js with api

--- src/main/webapp/contactos/app.js
+++ src/main/webapp/contactos/app.js
@@ -1,6 +1,4 @@
-const API_URL = window.location.pathname.includes('/contactos-crud-jakartaee/api/vuelos')
-    ? window.location.pathname.split('/contactos-crud-jakartaee/api/vuelos')[0] + '/contactos-crud-jakartaee/api/vuelos'
-    : '/contactos-crud-jakartaee/api/vuelos';
+const API_URL = '/api/vuelos';
 
 document.addEventListener('DOMContentLoaded', () => {
     const tablaBody = document.getElementById('tabla-vuelos');
